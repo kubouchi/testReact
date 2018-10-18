78e24caa98a1c474825a6c20e8e8ecd9767a0d3c
kubouchi <>
2018-10-18 18:34:17 +0900

    add vue application

---
+++ vue-sample-app/src/App.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  //- <!-- <div id="app">
+  //-   <img src="./assets/logo.png">
+  //-   <router-view/>
+  //- </div> -->
+  #app
+    img(src="./assets/logo.png")
+    .sample pugで記述
+    router-view
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  :root {
+    --sample-weight: 900;
+  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.sample {
+  font-weight: var(--sample-weight);
+}
+</style>
